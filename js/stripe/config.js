b0030700307bb47e839cff8f39484746e641a49a
Yoypi <ton@email.com>
2025-06-01 14:11:23 +0100

    Correction des URLs de redirection Stripe pour le site déployé

--- js/stripe/config.js
+++ js/stripe/config.js
@@ -3,8 +3,8 @@ const stripeConfig = {
   publicKey: 'pk_test_51RVBOjFwpGRfv8Xb4XQKQDKIUqx9o4FWRCPVI5PkumEZqAs6u6n6cbYSZWxM6otB3MI4pWUTqIuU2mdRDXjb7awK00WcE9g2zw',
   currency: 'eur',
   locale: 'fr',
-  successUrl: window.location.origin + '/confirmation.html',
-  cancelUrl: window.location.origin + '/checkout.html'
+  successUrl: 'https://samouraishop.netlify.app/confirmation.html',
+  cancelUrl: 'https://samouraishop.netlify.app/checkout.html'
 };
 
 // Exporter la configuration
