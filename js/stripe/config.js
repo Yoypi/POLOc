a08f1251973493410249f6cef143c90bc52eb266
Yoypi <ton@email.com>
2025-06-01 13:58:06 +0100

    Mise à jour de la clé API Stripe

--- js/stripe/config.js
+++ js/stripe/config.js
@@ -1,6 +1,6 @@
 // Configuration Stripe
 const stripeConfig = {
-  publicKey: 'pk_test_51OtDzwGANkQM6xUPmBGlcRozIJ4Q5R1XxXG73vQUfHZLEpg78rrNZc9Xzk8gDnXOoBp3R2ZpnJLs3CXOBHc4n97I00R0I6XQ6j',
+  publicKey: 'pk_test_51RVBOjFwpGRfv8Xb4XQKQDKIUqx9o4FWRCPVI5PkumEZqAs6u6n6cbYSZWxM6otB3MI4pWUTqIuU2mdRDXjb7awK00WcE9g2zw',
   currency: 'eur',
   locale: 'fr',
   successUrl: window.location.origin + '/confirmation.html',
